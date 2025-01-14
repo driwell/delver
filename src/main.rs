15c97624d215e60c1e900edd6fa0b142fc70be62
driwell <driwell@tuta.io>
2025-01-14 16:56:11 +0000

    add a field and the player

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::{prelude::*, window::WindowMode};
 
+#[derive(Component)]
+struct Player;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -10,5 +13,28 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(Startup, (setup_scene, setup_camera))
         .run();
+}
+
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn((
+        Mesh2d(meshes.add(Rectangle::new(1600., 960.))),
+        MeshMaterial2d(materials.add(Color::srgb(0.2, 0.2, 0.3))),
+    ));
+
+    commands.spawn((
+        Player,
+        Mesh2d(meshes.add(Circle::new(25.))),
+        MeshMaterial2d(materials.add(Color::srgb(1., 1., 1.))),
+        Transform::from_xyz(0., 0., 2.),
+    ));
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
