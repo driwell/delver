b66e17f011a07c2fe5c8f2c3283231d8cf25928c
driwell <driwell@tuta.io>
2025-01-16 17:20:02 +0000

    add player movement through controls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::{prelude::*, window::WindowMode};
 
+const PLAYER_SPEED: f32 = 150.;
+
 #[derive(Component)]
 struct Player;
 
@@ -14,6 +16,7 @@ fn main() {
             ..default()
         }))
         .add_systems(Startup, (setup_scene, setup_camera))
+        .add_systems(Update, move_player)
         .run();
 }
 
@@ -37,4 +40,35 @@ fn setup_scene(
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
+}
+
+fn move_player(
+    mut player: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+    kb_input: Res<ButtonInput<KeyCode>>,
+) {
+    let Ok(mut player) = player.get_single_mut() else {
+        return;
+    };
+
+    let mut direction = Vec2::ZERO;
+
+    if kb_input.pressed(KeyCode::KeyW) {
+        direction.y += 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyS) {
+        direction.y -= 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyA) {
+        direction.x -= 1.;
+    }
+
+    if kb_input.pressed(KeyCode::KeyD) {
+        direction.x += 1.;
+    }
+
+    let move_delta = direction.normalize_or_zero() * PLAYER_SPEED * time.delta_secs();
+    player.translation += move_delta.extend(0.);
 }
